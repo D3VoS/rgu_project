54f757eadf73e2d6e43025575333fcfea20edb06
=D3V <davidardow@gmail.com>
2020-10-23 22:23:05 +0100

    Initial Commit
    Initial Commit

---
+++ app/src/main/java/com/david/project_rgu/ViewExerciseActivity.java
@@ -0,0 +1,14 @@
+package com.david.project_rgu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_exercise);
+    }
+}
